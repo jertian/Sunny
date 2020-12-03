2828359deed5fe87e0a2e252da4ed9e5e9658753
jertian <jeremytian@cmail.carleton.ca>
2020-12-03 16:47:54 -0500

    Chart logic

--- Components/Common/ServerInfo.js
+++ Components/Common/ServerInfo.js
@@ -1,5 +1,6 @@
-var ip = "192.168.1.9"; //James' IP
+//var ip = "192.168.1.9"; //James' IP
 //var ip = "192.168.1.174"; //Paulina Ip
+var ip = "192.168.50.50"; //Jeremy IP
 var path = "http://" + ip + ":5000"; //Create path with port number
 
 module.exports = {
